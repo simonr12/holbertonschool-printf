9db185c856d8080e7897462535ba13e7150b9ccf
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-25 15:09:54 +0100

    refactor _printf function for improved readability and consistency

--- _printf2.c
+++ _printf2.c
@@ -1,62 +1,69 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
 /**
  * _printf - Custom printf implementation
- * @format: Format string containing the characters and format specifiers
+ * @format: Format string containing characters and format specifiers
+ *
  * Return: Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-    va_list arg_list;
-    int i = 0;
-    int len = 0;
+	va_list arg_list;
+	int i = 0;
+	int len = 0;
 
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    va_start(arg_list, format);
+	if (format == NULL)
+		return (-1);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%' && format[i + 1] == '%') // PErcent charater
-            {
-                putchar('%');
-                i += 2;
-                len++;
-            }
-        else if (format[i] == '%' && format[i + 1] == 'c') //Print character
-        {
-            char c = (char)va_arg(arg_list, int);
-            putchar(c);
-            i += 2;
-            len++;
-        }
-        else if (format[i] == '%' && format[i + 1] == 's') // Print strings
-        {
-            char *s = va_arg(arg_list, char *);
-            if (s == NULL)
-                s = "(null)";
-            while (*s != '\0')
-            {
-                putchar(*s);
-                s++;
-                len++;
-            }
-            i += 2;
-        }
-        else
-        {
-            putchar(format[i]);
-            i++;
-            len++;
-        }
-    }
+	va_start(arg_list, format);
 
-    va_end(arg_list);
-    return (len); // Length of the format string
+	while (format[i] != '\0')
+	{
+		/* Print % character */
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			putchar('%');
+			i += 2;
+			len++;
+		}
+		/* Print character */
+		else if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			char c = (char)va_arg(arg_list, int);
+
+			putchar(c);
+			i += 2;
+			len++;
+		}
+		/* Print string */
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			char *s = va_arg(arg_list, char *);
+
+			if (s == NULL)
+				s = "(null)";
+
+			while (*s != '\0')
+			{
+				putchar(*s);
+				s++;
+				len++;
+			}
+			i += 2;
+		}
+		/* Print regular characters */
+		else
+		{
+			putchar(format[i]);
+			i++;
+			len++;
+		}
+	}
+
+	va_end(arg_list);
+	return (len);
 }
