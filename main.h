ce0f729277c2345813de40bcccd477c8dc50034d
Nab Z <zinini.nabil@gmail.com>
2025-11-26 10:27:50 +0100

    Update Main.h

--- main.h
+++ main.h
@@ -2,10 +2,21 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <unistd.h>
+
+/**
+ * struct checker - maps specifier to handler function
+ * @type: format specifier (e.g. 'c', 's', '%')
+ * @func: function to handle printing
+ */
+typedef struct checker
+{
+	char *type;
+	int (*func)(va_list);
+} checker;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-
+int print_char(va_list list);
+int print_string(va_list list);
+int print_100(va_list list);
 
 #endif
